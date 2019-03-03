db26755635ac6309e3aac03138cb06835adc68b2
Ognjen Pejovic <opejovic@gmail.com>
2019-03-03 22:08:25 +0100

    20.2 The first update test

--- tests/Feature/Backstage/EditConcerTest.php
+++ tests/Feature/Backstage/EditConcerTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature\Backstage;
 
 use App\Models\Concert;
 use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestResponse;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -89,5 +90,58 @@ class EditConcerTest extends TestCase
 
         $response->assertStatus(302);
         $response->assertRedirect('/login');
+    }
+
+    /** @test */
+    function promoters_can_edit_their_own_unpublished_concerts()
+    {
+        $this->withoutExceptionHandling();
+
+        $user = factory(User::class)->create();
+        $concert = factory(Concert::class)->create([
+            'user_id' => $user->id,
+            'title' => 'Old title',
+            'subtitle' => 'Old subtitle',
+            'date' => Carbon::parse('2017-01-01 8:00pm'),
+            'ticket_price' => 2000,
+            'venue' => 'Old venue name',
+            'venue_address' => 'Old venue address',
+            'city' => 'Old city',
+            'state' => 'Old state',
+            'zip' => '000000',
+            'additional_information' => 'Old additional information.',    
+        ]);
+        $this->assertFalse($concert->isPublished());
+
+        $response = $this->actingAs($user)->patch("/backstage/concerts/{$concert->id}", [
+            'title' => 'New title',
+            'subtitle' => 'New subtitle',
+            'date' => '2020-01-01',
+            'time' => '9:00pm',
+            'ticket_price' => '25.00',
+            'venue' => 'New venue name',
+            'venue_address' => 'New venue address',
+            'city' => 'New city',
+            'state' => 'New state',
+            'zip' => '11111',
+            'additional_information' => 'New additional information.',    
+        ]);
+
+        $response->assertStatus(200);
+        $response->assertRedirect("/backstage/concerts");
+
+        tap($concert->fresh(), function ($concert) {
+            $this->assertEquals('New title', $concert->title);
+            $this->assertEquals('New subtitle', $concert->subtitle);
+            $this->assertEquals('New subtitle', $concert->subtitle);
+            $this->assertEquals(Carbon::parse('2020-01-01 9:00pm'), $concert->date);
+            $this->assertEquals('25.00', $concert->ticket_price_in_dollars);
+            $this->assertEquals('New venue name', $concert->venue);
+            $this->assertEquals('New venue address', $concert->venue_address);
+            $this->assertEquals('New city', $concert->city);
+            $this->assertEquals('New state', $concert->state);
+            $this->assertEquals('11111', $concert->zip);
+            $this->assertEquals('New additional information.', $concert->additional_information);
+        });
     }
 }
